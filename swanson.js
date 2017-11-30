3802654a92c5dfa82d707a599c2b5ee9e882b669
Moe Sadoon <msadoon590@gmail.com>
2017-11-30 22:44:09 +0000

    added jQuery and axios

--- swanson.js
+++ swanson.js
@@ -1,11 +1,12 @@
-'use strict';
-
 const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 const quoteTxt = document.getElementById('quote');
+const xhrBtn = document.getElementById('xhr');
+const fetchBtn = document.getElementById('fetch');
+const jQueryBtn = document.getElementById('jquery');
+const axiosBtn = document.getElementById('axios');
 
-
-//Using XML HTTP Requests (XHR button):
-document.getElementById('xhr').addEventListener("click", () => {
+//XHR
+xhrBtn.addEventListener("click", () => {
   let XHR = new XMLHttpRequest();
   XHR.onreadystatechange = function() {
     if(XHR.readyState == 4 && XHR.status == 200) {
@@ -17,20 +18,42 @@ document.getElementById('xhr').addEventListener("click", () => {
   XHR.send();
 })
 
-//Using Fetch API (Fetch button):
-document.getElementById('fetch').addEventListener("click", () => {
-    fetch(url)
-    .then(function(response) {
-      if(!response.ok) {
-        throw Error(response.status)
-      }
-      let data = response.json();
-      return data
-    })
-    .then(function(data) {
-      quoteTxt.innerText = data[0];
-    })
-    .catch(function(err) {
-      console.log(err);
-    })
+//Fetch
+fetchBtn.addEventListener("click", () => {
+  fetch(url)
+  .then(function(response) {
+    if(!response.ok) {
+      throw Error(response.status)
+    }
+    let data = response.json();
+    return data
   })
+  .then(function(data) {
+    quoteTxt.innerText = data[0];
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+})
+
+//jQuery
+$(jQueryBtn).click( () => {
+  $.ajax({
+    method: "GET",
+    url: url,
+    // dataType: "json"
+  })
+  .done(function(response) {
+    $(quoteTxt).text(response[0])
+  })
+  .fail(function() {
+    console.log("There was an error");
+  })
+})
+
+//Axios
+axiosBtn.addEventListener("click", () => {
+  axios.get(url)
+  .then(response => quoteTxt.innerText = response.data)
+  .catch(() => console.log("there was an error"));
+});
